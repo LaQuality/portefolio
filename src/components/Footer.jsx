fbb78ea1f69b1752787a1cbbe4264c52c9aece24
Kiko Johnny <josuekiko3@gmail.com>
2026-01-21 15:16:00 +0100

    Version 6

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,20 @@
 export default function Footer() {
   return (
     
-     <footer className="flex flex-col items-center bg-black text-gray-100 h-20  ombre  mt-20">       
+     <footer className="flex flex-col items-center bg-black text-gray-100 h-18  ombre  mt-20">       
          {/* <a href="https://fr.freepik.com/photos-gratuite/illustration-3d-amusante-arbitre-noir-masque_14201303.htm#fromView=search&page=1&position=2&uuid=4b6495cf-23a9-4c14-bc31-a49d6513ac83&query=avatar+dessin+sans+arri%C3%A8re+plan" className="m-1 mt-2 underline">Image de julos sur Freepik</a> */}
-         <a href="https://fr.freepik.com/vecteurs-libre/garcon-qui-etudie-ordinateur-portable-illustration_88805085.htm#fromView=search&page=2&position=13&uuid=e2b52817-4667-456a-ba0e-e01d19726549&query=personnage+informaticien+dessin " className="m-1 mb-4 underline">Image de djvstock sur Freepik</a>
+        
+        
+        
+         <div className="dropdown dropdown-top  m-1 mb-4 underline ">
+            <div tabIndex={0} role="button" className="cursor-pointer">Credits Images</div>
+            <ul tabIndex="-1" className="dropdown-content menu rounded-box z-1 w-52 p-2 shadow-sm bg-black">
+              <li> <a href="https://fr.freepik.com/vecteurs-libre/garcon-qui-etudie-ordinateur-portable-illustration_88805085.htm#fromView=search&page=2&position=13&uuid=e2b52817-4667-456a-ba0e-e01d19726549&query=personnage+informaticien+dessin ">Image de djvstock sur Freepik</a></li>
+              <li> <a href="https://www.freepik.com/free-photo/close-up-hand-with-magnifying-glass_13662469.htm#fromView=search&page=1&position=24&uuid=6eb48255-4d59-470c-bd33-f30a5cee4d73&query=recherche+">Image by freepik</a></li>
+              <li> <a href="https://www.freepik.com/free-photo/kids-participating-treasure-hunt_16621685.htm#fromView=search&page=1&position=42&uuid=6d34ba8f-6846-471b-8971-dce8b44c86b3&query=jeux+de+recherche">Image by freepik</a></li>
+            </ul>
+        </div>
+         
          <span className="text-sm">La Qualité (Johnny Kiko) © 2026</span> 
      </footer>  
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,33 +1,55 @@
 import { AiOutlineClose } from "react-icons/ai";
 import reactLogo from "../../public/skills/react.svg"
+import html from "../../public/skills/html.svg"
+import figma from "../../public/skills/figma.svg"
+import css from "../../public/skills/css.svg"
+import tailwind from "../../public/skills/tailwind.svg"
+
+
+
+
 
 export default function Skills() {
+
+    const skills = [
+        {image : reactLogo, name : "React Js"},
+        {image : html, name : "HTML"},
+        {image : css, name : "CSS"},
+        {image : tailwind, name : "Tailwind CSS"},
+        {image : figma, name : "Figma"}
+
+    ]
+
+
+
   return (
    
      <dialog id="my_modal_4" className="modal z-10 backdrop-blur-sm flex flex-col justify-center  text-white">
-  <div className="modal-box w-11/12 max-w-5xl h-[80vh] bg-black border border-orange-500 shadow-xl shadow-black text-center">
-    <h3 className="font-bold text-4xl">Skills</h3>
-    <p className="py-4">Click the button below to close</p>
+            <div className="modal-box w-11/12 max-w-5xl h-[75vh] md:[80vh] bg-black border border-orange-500 shadow-xl shadow-black text-center">
+                <h3 className="font-bold text-4xl">Skills</h3>
+                <p className="py-4">Click the button below to close</p>
 
-    <div className="grid grid-cols-1 md:grid-cols-3">
+                <div className="grid grid-cols-1 md:grid-cols-3">
+                   
+                    {skills.map((skill,index) => (
 
-           <div className="flex flex-col items-center">
-                <img src={reactLogo} className="h-30 md:h-35" alt="logo" />
-                 <span className="font-bold">React JS</span>
-           </div>
+                         <div className="flex flex-col items-center" key={index}>
+                            <img src={skill.image} className="h-25  w-30" alt="logo" />
+                            <span className="font-bold mt-2 mb-5">{skill.name}</span>
+                         </div>
 
-           
+                    ))}
 
 
-    </div>
-   
-    </div>
-     <div className="modal-action w-full flex justify-center">
-      <form method="dialog">
-        {/* if there is a button, it will close the modal */}
-        <button className=" bg-orange-500 p-4 cursor-pointer rounded-full font-bold "><AiOutlineClose className="" /></button>
-      </form>
-  </div>
-</dialog>
+                </div>
+            
+                </div>
+                <div className="modal-action w-full flex justify-center">
+                <form method="dialog">
+                    {/* if there is a button, it will close the modal */}
+                    <button className=" bg-orange-500 p-4 cursor-pointer rounded-full font-bold "><AiOutlineClose className="" /></button>
+                </form>
+            </div>
+     </dialog>
   )
 }
