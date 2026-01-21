74794ab0a9fea26541ba796d718084f699d43054
Kiko Johnny <josuekiko3@gmail.com>
2026-01-21 13:39:58 +0100

    Version 5

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import { AiOutlineClose } from "react-icons/ai";
+import reactLogo from "../../public/skills/react.svg"
 
 export default function Skills() {
   return (
    
-     <dialog id="my_modal_4" className="modal z-10 flex flex-col justify-center  text-white">
-  <div className="modal-box w-11/12 max-w-5xl bg-black border border-orange-500 text-center">
+     <dialog id="my_modal_4" className="modal z-10 backdrop-blur-sm flex flex-col justify-center  text-white">
+  <div className="modal-box w-11/12 max-w-5xl h-[80vh] bg-black border border-orange-500 shadow-xl shadow-black text-center">
     <h3 className="font-bold text-4xl">Skills</h3>
     <p className="py-4">Click the button below to close</p>
+
+    <div className="grid grid-cols-1 md:grid-cols-3">
+
+           <div className="flex flex-col items-center">
+                <img src={reactLogo} className="h-30 md:h-35" alt="logo" />
+                 <span className="font-bold">React JS</span>
+           </div>
+
+           
+
+
+    </div>
    
     </div>
      <div className="modal-action w-full flex justify-center">
       <form method="dialog">
         {/* if there is a button, it will close the modal */}
-        <button className=" bg-orange-500 p-3 cursor-pointer rounded-lg font-bold ">Close</button>
+        <button className=" bg-orange-500 p-4 cursor-pointer rounded-full font-bold "><AiOutlineClose className="" /></button>
       </form>
   </div>
 </dialog>
